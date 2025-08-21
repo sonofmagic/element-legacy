767a863caf7681c7854ee3d1da03625effbcb266
ice breaker <1324318532@qq.com>
2025-08-21 13:02:59 +0800

    chore: use sass

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const { series, src, dest } = require('gulp');
-const sass = require('gulp-sass');
+const gulpSass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
-
+const dartSass = require('sass');
+const sass = gulpSass(dartSass);
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass.sync())
+    .pipe(sass.sync(
+      {
+        silenceDeprecations: ['legacy-js-api', 'import', 'slash-div', 'global-builtin']
+      }
+    ))
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
