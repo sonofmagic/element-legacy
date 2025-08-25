95557a7a4cd1a093c032fba94d220b1ed33b4944
ice breaker <1324318532@qq.com>
2025-08-25 15:03:23 +0800

    chore: add demo

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -3,6 +3,7 @@ import Vue from '@vitejs/plugin-vue2'
 import VueJsx from '@vitejs/plugin-vue2-jsx'
 import path from 'path'
 import Markdown from 'unplugin-vue-markdown/vite'
+import markdownItContainer from 'markdown-it-container'
 
 export default defineConfig({
     resolve: {
@@ -19,7 +20,41 @@ export default defineConfig({
         ),
         VueJsx(),
         Markdown({
-            vueVersion: '2.7'
+            vueVersion: '2.7',
+            markdownItSetup(md) {
+                md.use(markdownItContainer, 'demo', {
+                    validate(params) {
+                        return params.trim().match(/^demo\s*(.*)$/)
+                    },
+                    render(tokens, idx) {
+                        const m = tokens[idx].info.trim().match(/^demo\s*(.*)$/)
+
+                        if (tokens[idx].nesting === 1) {
+                            // 容器开始
+                            const description = m && m.length > 1 ? m[1] : ''
+                            const nextIndex = idx + 1
+                            const codeToken = tokens[nextIndex]
+
+                            // 从 tokens 中获取源码字符串
+                            let sourceCode = ''
+                            if (codeToken.type === 'fence' && codeToken.info.trim().startsWith('vue')) {
+                                sourceCode = codeToken.content
+                            }
+
+                            // 传给 Demo 组件
+                            const escaped = sourceCode
+                                .replace(/</g, '&lt;')
+                                .replace(/>/g, '&gt;')
+                                .replace(/`/g, '&#96;')
+
+                            return `<Demo source="${JSON.stringify(escaped)}" description="${description}">\n`
+                        } else {
+                            // 容器结束
+                            return '</Demo>\n'
+                        }
+                    }
+                })
+            }
         })
     ],
     css: {
