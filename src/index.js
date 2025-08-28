2b71a2eb5ae66b5a34819a4d88ad752eb5d40e4e
ice breaker <1324318532@qq.com>
2025-08-28 13:07:33 +0800

    chore(deps): upgrade

--- src/index.js
+++ src/index.js
@@ -212,7 +212,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '3.0.0',
+  version: '3.0.0-alpha.0',
   locale: locale.use,
   i18n: locale.i18n,
   install,
