e489b372d7f64f60973e949fceaab708ea783b64
ice breaker <1324318532@qq.com>
2025-08-28 14:45:42 +0800

    chore: bump version

--- build/config.js
+++ build/config.js
@@ -1,49 +1,49 @@
-var path = require('path');
-var fs = require('fs');
-var nodeExternals = require('webpack-node-externals');
-var Components = require('../components.json');
+let fs = require('node:fs')
+let path = require('node:path')
+let nodeExternals = require('webpack-node-externals')
+let Components = require('../components.json')
 
-var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'));
-var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
-var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'));
-var externals = {};
+let utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'))
+let mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'))
+let transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'))
+let externals = {}
 
-Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
-});
+Object.keys(Components).forEach((key) => {
+  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`
+})
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
-utilsList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
-});
-mixinsList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
-});
-transitionList.forEach(function(file) {
-  file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
-});
+externals['element-ui/src/locale'] = 'element-ui/lib/locale'
+utilsList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`
+})
+mixinsList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`
+})
+transitionList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`
+})
 
 externals = [Object.assign({
-  vue: 'vue'
-}, externals), nodeExternals()];
+  vue: 'vue',
+}, externals), nodeExternals()]
 
-exports.externals = externals;
+exports.externals = externals
 
 exports.alias = {
-  main: path.resolve(__dirname, '../src'),
-  packages: path.resolve(__dirname, '../packages'),
-  examples: path.resolve(__dirname, '../examples'),
-  'element-ui': path.resolve(__dirname, '../')
-};
+  'main': path.resolve(__dirname, '../src'),
+  'packages': path.resolve(__dirname, '../packages'),
+  'examples': path.resolve(__dirname, '../examples'),
+  'element-ui': path.resolve(__dirname, '../'),
+}
 
 exports.vue = {
   root: 'Vue',
   commonjs: 'vue',
   commonjs2: 'vue',
-  amd: 'vue'
-};
+  amd: 'vue',
+}
 
-exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js|utils\/lodash\.js/;
+exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js|utils\/lodash\.js/
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
+import fs from 'node:fs'
 import path from 'node:path'
 import Vue from '@vitejs/plugin-vue2'
 import VueJsx from '@vitejs/plugin-vue2-jsx'
 import { defineConfig } from 'vite'
 import entry from './components.json'
 
+const utilsList = fs.readdirSync(path.resolve(__dirname, './src/utils'))
+const mixinsList = fs.readdirSync(path.resolve(__dirname, './src/mixins'))
+const transitionList = fs.readdirSync(path.resolve(__dirname, './src/transitions'))
+let externals: Record<string, string> = {}
+
+Object.keys(entry).forEach((key) => {
+  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`
+})
+
+externals['element-ui/src/locale'] = 'element-ui/lib/locale'
+utilsList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`
+})
+mixinsList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`
+})
+transitionList.forEach((file) => {
+  file = path.basename(file, '.js')
+  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`
+})
+
+// externals = [Object.assign({
+//   vue: 'vue',
+// }, externals)]
+
 export default defineConfig({
   resolve: {
     alias: {
       'element-ui': path.resolve(import.meta.dirname),
+      // ...externals,
     },
   },
   plugins: [
@@ -23,14 +52,19 @@ export default defineConfig({
       },
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ['vue', ...Object.keys(externals)],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           vue: 'Vue',
         },
+        paths: {
+          ...externals,
+        },
+        // interop: 'default',
       },
     },
+    minify: false,
   },
 })
