1543924b025636040b20bc5b24e62bc00a61da25
ice breaker <1324318532@qq.com>
2025-10-15 14:45:36 +0800

    feat: format scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,15 @@ export default icebreaker(
   {
     rules: {
       'selector-class-pattern': null,
+      'nesting-selector-no-missing-scoping-root': null,
+      'selector-no-vendor-prefix': null,
+      'selector-pseudo-element-no-unknown': null,
+      'selector-pseudo-class-no-unknown': null,
+      'no-empty-source': null,
+      'no-descending-specificity': null,
+      'scss/selector-no-redundant-nesting-selector': null,
+      'scss/at-function-pattern': null,
+      'scss/dollar-variable-pattern': null,
     },
   },
 )
