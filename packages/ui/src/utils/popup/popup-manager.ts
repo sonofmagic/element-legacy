ad5ede8230db3684f538bfa20ad4ac70a6d68722
ice breaker <1324318532@qq.com>
2025-11-07 17:34:50 +0800

    chore: 全局单例popup-manager

--- packages/ui/src/utils/popup/popup-manager.ts
+++ packages/ui/src/utils/popup/popup-manager.ts
@@ -2,77 +2,89 @@
 import Vue from 'vue';
 import { addClass, removeClass } from 'element-ui/src/utils/dom';
 
-let hasModal = false;
-let hasInitZIndex = false;
-let zIndex;
+// Attach the manager to a global key so multiple bundles reuse one instance.
+const POPUP_MANAGER_KEY = '__ELEMENT_POPUP_MANAGER__';
 
-const getModal = function() {
-  if (Vue.prototype.$isServer) return;
-  let modalDom = PopupManager.modalDom;
-  if (modalDom) {
-    hasModal = true;
-  } else {
-    hasModal = false;
-    modalDom = document.createElement('div');
-    PopupManager.modalDom = modalDom;
-
-    modalDom.addEventListener('touchmove', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-    });
-
-    modalDom.addEventListener('click', function() {
-      PopupManager.doOnModalClick && PopupManager.doOnModalClick();
-    });
-  }
-
-  return modalDom;
+const getGlobalContext = function() {
+  if (typeof globalThis !== 'undefined') return globalThis;
+  if (typeof window !== 'undefined') return window;
+  if (typeof global !== 'undefined') return global;
+  return undefined;
 };
 
-const instances = {};
+const createPopupManager = function() {
+  let hasModal = false;
+  let hasInitZIndex = false;
+  let zIndex;
 
-const PopupManager = {
-  modalFade: true,
+  const instances = {};
 
-  getInstance: function(id) {
-    return instances[id];
-  },
+  const manager = {
+    modalFade: true,
+    modalStack: [],
+    modalDom: undefined,
 
-  register: function(id, instance) {
-    if (id && instance) {
-      instances[id] = instance;
+    getInstance(id) {
+      return instances[id];
+    },
+
+    register(id, instance) {
+      if (id && instance) {
+        instances[id] = instance;
+      }
+    },
+
+    deregister(id) {
+      if (id) {
+        instances[id] = null;
+        delete instances[id];
+      }
+    },
+
+    nextZIndex() {
+      return manager.zIndex++;
+    },
+
+    doOnModalClick() {
+      const topItem = manager.modalStack[manager.modalStack.length - 1];
+      if (!topItem) return;
+
+      const instance = manager.getInstance(topItem.id);
+      if (instance && instance.closeOnClickModal) {
+        instance.close();
+      }
     }
-  },
+  };
 
-  deregister: function(id) {
-    if (id) {
-      instances[id] = null;
-      delete instances[id];
+  const getModal = function() {
+    if (Vue.prototype.$isServer) return;
+    let modalDom = manager.modalDom;
+    if (modalDom) {
+      hasModal = true;
+    } else {
+      hasModal = false;
+      modalDom = document.createElement('div');
+      manager.modalDom = modalDom;
+
+      modalDom.addEventListener('touchmove', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      });
+
+      modalDom.addEventListener('click', function() {
+        manager.doOnModalClick && manager.doOnModalClick();
+      });
     }
-  },
 
-  nextZIndex: function() {
-    return PopupManager.zIndex++;
-  },
+    return modalDom;
+  };
 
-  modalStack: [],
-
-  doOnModalClick: function() {
-    const topItem = PopupManager.modalStack[PopupManager.modalStack.length - 1];
-    if (!topItem) return;
-
-    const instance = PopupManager.getInstance(topItem.id);
-    if (instance && instance.closeOnClickModal) {
-      instance.close();
-    }
-  },
-
-  openModal: function(id, zIndex, dom, modalClass, modalFade) {
+  manager.openModal = function(id, zIndex, dom, modalClass, modalFade) {
     if (Vue.prototype.$isServer) return;
     if (!id || zIndex === undefined) return;
-    this.modalFade = modalFade;
+    manager.modalFade = modalFade;
 
-    const modalStack = this.modalStack;
+    const modalStack = manager.modalStack;
 
     for (let i = 0, j = modalStack.length; i < j; i++) {
       const item = modalStack[i];
@@ -84,7 +96,7 @@ const PopupManager = {
     const modalDom = getModal();
 
     addClass(modalDom, 'v-modal');
-    if (this.modalFade && !hasModal) {
+    if (manager.modalFade && !hasModal) {
       addClass(modalDom, 'v-modal-enter');
     }
     if (modalClass) {
@@ -107,11 +119,11 @@ const PopupManager = {
     modalDom.tabIndex = 0;
     modalDom.style.display = '';
 
-    this.modalStack.push({ id: id, zIndex: zIndex, modalClass: modalClass });
-  },
+    modalStack.push({ id: id, zIndex: zIndex, modalClass: modalClass });
+  };
 
-  closeModal: function(id) {
-    const modalStack = this.modalStack;
+  manager.closeModal = function(id) {
+    const modalStack = manager.modalStack;
     const modalDom = getModal();
 
     if (modalStack.length > 0) {
@@ -137,59 +149,71 @@ const PopupManager = {
     }
 
     if (modalStack.length === 0) {
-      if (this.modalFade) {
+      if (manager.modalFade) {
         addClass(modalDom, 'v-modal-leave');
       }
       setTimeout(() => {
         if (modalStack.length === 0) {
           if (modalDom.parentNode) modalDom.parentNode.removeChild(modalDom);
           modalDom.style.display = 'none';
-          PopupManager.modalDom = undefined;
+          manager.modalDom = undefined;
         }
         removeClass(modalDom, 'v-modal-leave');
       }, 200);
     }
-  }
-};
+  };
 
-Object.defineProperty(PopupManager, 'zIndex', {
-  configurable: true,
-  get() {
-    if (!hasInitZIndex) {
-      zIndex = zIndex || (Vue.prototype.$ELEMENT || {}).zIndex || 2000;
-      hasInitZIndex = true;
-    }
-    return zIndex;
-  },
-  set(value) {
-    zIndex = value;
-  }
-});
-
-const getTopPopup = function() {
-  if (Vue.prototype.$isServer) return;
-  if (PopupManager.modalStack.length > 0) {
-    const topPopup = PopupManager.modalStack[PopupManager.modalStack.length - 1];
-    if (!topPopup) return;
-    const instance = PopupManager.getInstance(topPopup.id);
-
-    return instance;
-  }
-};
-
-if (!Vue.prototype.$isServer) {
-  // handle `esc` key when the popup is shown
-  window.addEventListener('keydown', function(event) {
-    if (event.keyCode === 27) {
-      const topPopup = getTopPopup();
-
-      if (topPopup && topPopup.closeOnPressEscape) {
-        topPopup.handleClose
-          ? topPopup.handleClose()
-          : (topPopup.handleAction ? topPopup.handleAction('cancel') : topPopup.close());
+  Object.defineProperty(manager, 'zIndex', {
+    configurable: true,
+    get() {
+      if (!hasInitZIndex) {
+        zIndex = zIndex || (Vue.prototype.$ELEMENT || {}).zIndex || 2000;
+        hasInitZIndex = true;
       }
+      return zIndex;
+    },
+    set(value) {
+      zIndex = value;
     }
   });
+
+  const getTopPopup = function() {
+    if (Vue.prototype.$isServer) return;
+    if (manager.modalStack.length > 0) {
+      const topPopup = manager.modalStack[manager.modalStack.length - 1];
+      if (!topPopup) return;
+      const instance = manager.getInstance(topPopup.id);
+
+      return instance;
+    }
+  };
+
+  if (!Vue.prototype.$isServer && typeof window !== 'undefined') {
+    // handle `esc` key when the popup is shown
+    window.addEventListener('keydown', function(event) {
+      if (event.keyCode === 27) {
+        const topPopup = getTopPopup();
+
+        if (topPopup && topPopup.closeOnPressEscape) {
+          topPopup.handleClose
+            ? topPopup.handleClose()
+            : (topPopup.handleAction ? topPopup.handleAction('cancel') : topPopup.close());
+        }
+      }
+    });
+  }
+
+  return manager;
+};
+
+const globalContext = getGlobalContext();
+let PopupManager = globalContext ? globalContext[POPUP_MANAGER_KEY] : undefined;
+
+if (!PopupManager) {
+  PopupManager = createPopupManager();
+  if (globalContext) {
+    globalContext[POPUP_MANAGER_KEY] = PopupManager;
+  }
 }
 
 export default PopupManager;
